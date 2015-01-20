9962001ece25be03e24bae1f2f15d306320b2f08
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2015-01-20 16:26:27 -0200

    Refactor CustomerRequest

--- reservation/customer.py
+++ reservation/customer.py
@@ -1,13 +1,47 @@
-class CustomerRequest:
-  """A customer's request data"""
-  def __init__(self, customer_type, dates):
+
+class CustomerRequest(object):
+  """ Customer's request data. """
+
+  def __init__(self, customer_type, request_dates):
     self.customer_type = customer_type
-    self.dates = dates
+
+    self._request_dates = request_dates
+    self._weekday_count = 0
+    self._weekend_count = 0
+
+    self._update_days_count()
+
+
+  def get_request_dates(self):
+    return self._request_dates
+
+  def set_request_dates(self, request_dates):
+    self._request_dates = request_dates
+    self._update_days_count()
+
+  def get_weekday_count(self):
+    return self._weekday_count
+
+  def get_weekend_count(self):
+    return self._weekend_count
 
   def is_rewards(self):
     if(self.customer_type == "Rewards"):
       return True
     else:
       return False
+
+
+  # Protected methods
+
+  def _update_days_count(self):
+    self._weekday_count = 0
+    self._weekend_count = 0
+
+    for date in self._request_dates:
+      if "sat" in date or "sun" in date:
+        self._weekend_count += 1
+      else:
+        self._weekday_count += 1
 
 
