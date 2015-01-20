9e76af64beb8836560be7d590742ee36454765b3
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2015-01-20 16:27:40 -0200

    Refactor application

--- application.py
+++ application.py
@@ -1,14 +1,15 @@
+
 from reservation import inputs
 from reservation import hotels
 
 hotel_chain = hotels.HotelChain()
 hotel_chain.load_hotels('hotels.csv')
 
-input_parser = inputs.TextFileReader()
-input_parser.set_input_source('inputs.txt')
-input_object = input_parser.get_next()
+input_reader = inputs.TextFileReader()
+input_reader.set_input_source('inputs.txt')
+customer_request = input_reader.get_next()
 
-while(input_object):
-  print hotel_chain.find_cheapest(input_object)
-  input_object = input_parser.get_next()
+while(customer_request):
+  print hotel_chain.find_cheapest(customer_request)
+  customer_request = input_reader.get_next()
 
