336a0e0494fe0b8aed4ee0a45ca9af7f5c88f6c9
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2015-01-20 16:46:14 -0200

    Add a test stub

---
+++ tests/test_customer.py
@@ -0,0 +1,18 @@
+
+import unittest
+from reservation import customer
+ 
+class TestCustomer(unittest.TestCase):
+ 
+    def setUp(self):
+        pass
+ 
+    def weekdays(self):
+        self.assertEqual(4, 12)
+
+    def weekends(self):
+        self.assertEqual(4, 12)
+ 
+if __name__ == '__main__':
+    unittest.main()
+
