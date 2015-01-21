878c594695f8b37caea4f1a8702d606034e3c500
Guilherme Dias <guilhermeaugusto.dias@gmail.com>
2015-01-21 03:24:24 -0200

    Unit tests

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -6,13 +6,17 @@ class TestCustomer(unittest.TestCase):
  
     def setUp(self):
         pass
- 
-    def weekdays(self):
-        self.assertEqual(4, 12)
 
-    def weekends(self):
-        self.assertEqual(4, 12)
+    def tearDown(self):
+        pass
  
+    def test_weekday_1(self):
+        request = customer.CustomerRequest("Regular", ["16Mar2009(mon)", 
+                                                       "17Mar2009(tue)",
+                                                       "18Mar2009(wed)"])
+
+        self.assertEqual(request.get_weekday_count(), 3)
+
 if __name__ == '__main__':
     unittest.main()
 
